87552425da4d2d499ee0d501abffcef8a70dab33
Seyfullah Akgün <seyfullah.akgn@gmail.com>
2021-04-05 21:11:45 +0300

    ilk gün düzenlemesi

--- app/src/main/java/com/seyf/javabasics/Main.java
+++ app/src/main/java/com/seyf/javabasics/Main.java
@@ -3,5 +3,6 @@ package com.seyf.javabasics;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(5 * 2);
     }
 }
